2f6bcfc9638320056c308b3059b987f96fc60ed1
Bill Himmelsbach <whimmels@gmail.com>
2023-04-06 20:46:26 -0700

    feat(noclist): proof of concept, update tests and readme

--- noclist/README.md
+++ noclist/README.md
@@ -1,5 +1,6 @@
 TODO: Brainstorming:
 
+- dockerize?
 - potential errors
   - disconnect before connecting
   - disconnect while connecting
@@ -7,8 +8,9 @@ TODO: Brainstorming:
   - malformed data
   - unauthorized
   - 400, 401, 403, 404, 408, 500, 502, 504, 505, 507?, 508?
-- use same python version as CFPB?
-- dockerize?
+- python version?
+- run linter again
+- requests library using it without retry
 # noclist
 - [noclist](#noclist)
   - [Dev Environment](#dev-environment)
@@ -16,17 +18,26 @@ TODO: Brainstorming:
   - [Future Improvements](#future-improvements)
   - [noclist Documentation](#noclist-documentation)
 
+For this more backend oriented assessment question I wanted to use python, since the CFPB uses plenty of Django. Taking a peek at your [Dockerfile](https://github.com/cfpb/consumerfinance.gov/blob/main/Dockerfile) from your main repo, it looks like you're using a `python:3.8-alpine` image which runs python version 3.8.16. So that's what I'll be using here.
+
 TODO: brief description
 
 TODO: coming from Typescript land, I decided to add the optional typing
 
 TODO: gif of project
 
+TODO: Please write the retry logic yourself rather than use a request library with built-in retry functionality. Requests library ok, just not retry?
+
+TODO: no newline after stdout
+
+TODO: should this thing be wrapped in an object?
+
 ## Dev Environment
 
 ## Testing
 
 ## Future Improvements
+- add types to testing file
 
 ## noclist Documentation
 
--- noclist/main.py
+++ noclist/main.py
@@ -1,6 +1,94 @@
-import sys
+'''This module handles interfacing with the BADSEC API'''
 
-def get_noclist_main():
+import json
+import hashlib
+import sys
+from typing import Union, Optional, Mapping
+
+import requests
+
+# default docker address for adhocteam/noclist
+BADSEC_API_URL: str = 'http://0.0.0.0:8888'
+MAX_NUMBER_OF_CONNECTION_ATTEMPTS: int = 3
+
+
+def handle_exception(error: Exception) -> None:
+    """
+    abstracted error handling function, would use a log service like Sentry in production
+
+    """
+    print(error)
+
+
+def connect(url: str, headers: Optional[Mapping[str, Union[str, bytes]]]) -> requests.Response:
+    """
+    initiates initial API call with headers, retries API connection if connection fails
+    """
+    attempts: int = 0
+    while attempts < MAX_NUMBER_OF_CONNECTION_ATTEMPTS:
+        attempts += 1
+        try:
+            response: requests.Response = requests.get(url, headers=headers)
+            response.raise_for_status()
+            return response
+        except requests.exceptions.RequestException as error:
+            handle_exception(error)
+
+        # I don't believe the error exceptions below are required since all exceptions are caught by
+        # the above and the behavior of exiting is the same for all errors
+        # except requests.exceptions.HTTPError as error:
+        #     handle_exception(error)
+        # except requests.exceptions.ConnectionError as error:
+        #     handle_exception(error)
+        # except requests.exceptions.Timeout as error:
+        #     handle_exception(error)
+    # throw retry exception after max number of attempts (requests lib defaults to 1 try per call)
+    raise requests.exceptions.RetryError(
+        'Max number of connection attempts reached')
+
+
+def get_auth_token() -> str:
+    """
+    get authentication token from API
+    """
+    auth_url: str = BADSEC_API_URL + '/auth'
+    auth_response: requests.Response = connect(auth_url, None)
+    auth_token: str = auth_response.headers['Badsec-Authentication-Token']
+    return auth_token
+
+
+def get_auth_checksum(auth_token: str, endpoint: str) -> str:
+    """
+    gets the checksum necessary to authorize access the API
+    """
+    utf_8_string_to_be_hashed: bytes = (auth_token + endpoint).encode('utf-8')
+    sha_hash: hashlib.sha3_256 = hashlib.sha256(utf_8_string_to_be_hashed)
+    auth_checksum: str = sha_hash.hexdigest()
+    return auth_checksum
+
+
+def get_users(checksum: str) -> str:
+    """
+    get the list of users from the API
+    """
+    get_users_url: str = BADSEC_API_URL + '/users'
+    auth_header: dict[str, str] = {'X-Request-Checksum': checksum}
+    users_response: requests.Response = connect(
+        get_users_url, auth_header)
+    user_list: str = users_response.text
+    return user_list
+
+
+def get_noclist_main() -> None:
+    """
+    main function to get the noclist
+    """
+    auth_token: str = get_auth_token()
+    auth_checksum: str = get_auth_checksum(auth_token, '/users')
+    users_newline_delimited: str = get_users(auth_checksum)
+    users_json_string: str = json.dumps(users_newline_delimited.splitlines())
+    # using sys.stdout: assessment instructions say json string which don't include newline chars
+    sys.stdout.write(users_json_string)
     sys.exit(0)
 
 
